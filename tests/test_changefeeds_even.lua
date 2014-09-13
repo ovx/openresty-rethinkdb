ef4e60dd63d83e363073c1b5461ec3ca5cb2b415
Adam Grandquist <grandquista@gmail.com>
2014-09-13 14:49:49 -0700

    Only test direct ReQL function calls.

--- tests/test_changefeeds_even.lua
+++ tests/test_changefeeds_even.lua
@@ -2,7 +2,7 @@ r = require('rethinkdb')
 
 r.connect(function(err, c)
   r.db('changefeeds'):table('watched'):changes():filter(
-    (r.row['new_val']['id'] % 2).eq(0)
+    r.row:get_field('new_val'):get_field('id'):mod(2):eq(0)
   ):limit(2):run(
     c, function(err, cur)
       r.db('changefeeds'):table('watched'):insert(
