1ea72c590d5fd176d0da0cd74cd0356949839245
Adam Grandquist <grandquista@gmail.com>
2014-09-17 20:45:36 -0700

    Clean up error creation code.
    RqlQueryPrinter becomes a more normal class like the Python driver.

--- src/errors.lua
+++ src/errors.lua
@@ -5,7 +5,6 @@ do
   _base_0.__index = _base_0
   local _class_0 = setmetatable({
     __init = function(self, msg)
-      self.name = self.__name
       self.msg = msg
       self.message = msg
     end,
@@ -27,17 +26,18 @@ do
   _base_0.__index = _base_0
   local _class_0 = setmetatable({
     __init = function(self, msg, term, frames)
-      self.name = self.constructor.name
       self.msg = msg
       self.frames = frames
+      self.printer = RqlQueryPrinter(term, frames)
       if term then
-        if msg[msg.length - 1] == '.' then
-          self.message = tostring(msg.slice(0, msg.length - 1)) .. " in:\n" .. RqlQueryPrinter:printQuery(term) .. "\n" .. RqlQueryPrinter:printCarrots(term, frames)
+        self.message = " in:\n" .. self.printer:printQuery() .. "\n" .. self.printer:printCarrots()
+        if msg[-1] == '.' then
+          self.message = msg:sub(1, -2) .. self.message
         else
-          self.message = tostring(msg) .. " in:\n" .. RqlQueryPrinter:printQuery(term) .. "\n" .. RqlQueryPrinter:printCarrots(term, frames)
+          self.message = msg .. self.message
         end
       else
-        self.message = tostring(msg)
+        self.message = msg
       end
     end,
     __base = _base_0,
@@ -153,26 +153,81 @@ do
   RqlClientError = _class_0
 end
 do
-  local composeTerm, composeCarrots, carrotMarker, carrotify, joinTree
   local _base_0 = {
-    printQuery = function(term)
-      local tree = composeTerm(term)
-      return joinTree(tree)
+    printQuery = function(self)
+      return self:joinTree(self:composeTerm(self.term))
     end,
-    printCarrots = function(term, frames)
-      if frames.length == 0 then
-        local tree = {
-          carrotify(composeTerm(term))
+    printCarrots = function(self)
+      local tree
+      if self.frames.length == 0 then
+        tree = {
+          self:carrotify(self:composeTerm(self.term))
         }
       else
-        local tree = composeCarrots(term, frames)
+        tree = self:composeCarrots(self.term, self.frames)
       end
-      return joinTree(tree).gsub('[^\^]', ' ')
+      return self:joinTree(tree).gsub('[^\^]', ' ')
+    end,
+    composeTerm = function(self, term)
+      local args = {}
+      for i, arg in ipairs(term.args) do
+        args[i] = self:composeTerm(arg)
+      end
+      local optargs = { }
+      for key, arg in ipairs(term.optargs) do
+        optargs[key] = self:composeTerm(arg)
+      end
+      return term:compose(args, optargs)
+    end,
+    composeCarrots = function(self, term, frames)
+      local frame = frames.shift()
+      local args = {}
+      for arg, i in ipairs(term.args) do
+        if frame == i then
+          args[i] = self:composeCarrots(arg, frames)
+        else
+          args[i] = self:composeTerm(arg)
+        end
+      end
+      local optargs = { }
+      for key, arg in ipairs(term.optargs) do
+        if frame == key then
+          optargs[key] = self:composeCarrots(arg, frames)
+        else
+          optargs[key] = self:composeTerm(arg)
+        end
+      end
+      if frame then
+        return term.compose(args, optargs)
+      end
+      return self:carrotify(term.compose(args, optargs))
+    end,
+    carrotMarker = { },
+    carrotify = function(self, tree)
+      return {carrotMarker, tree}
+    end,
+    joinTree = function(self, tree)
+      local str = ''
+      for _, term in ipairs(tree) do
+        if Array.isArray(term) then
+          if term.length == 2 and term[0] == self.carrotMarker then
+            str = str .. self:joinTree(term[1]).gsub('.', '^')
+          else
+            str = str .. self:joinTree(term)
+          end
+        else
+          str = str .. term
+        end
+      end
+      return str
     end
   }
   _base_0.__index = _base_0
   local _class_0 = setmetatable({
-    __init = function(self) end,
+    __init = function(self, term, frames)
+      self.term = term
+      self.frames = frames
+    end,
     __base = _base_0,
     __name = "RqlQueryPrinter"
   }, {
@@ -185,75 +240,6 @@ do
   })
   _base_0.__class = _class_0
   local self = _class_0
-  composeTerm = function(term)
-    local args
-    do
-      local _accum_0 = { }
-      local _len_0 = 1
-      for i, arg in ipairs(term.args) do
-        _accum_0[_len_0] = composeTerm(arg)
-        _len_0 = _len_0 + 1
-      end
-      args = _accum_0
-    end
-    local optargs = { }
-    for key, arg in ipairs(term.optargs) do
-      optargs[key] = composeTerm(arg)
-    end
-    return term.compose(args, optargs)
-  end
-  composeCarrots = function(term, frames)
-    local frame = frames.shift()
-    local args
-    do
-      local _accum_0 = { }
-      local _len_0 = 1
-      for arg, i in ipairs(term.args) do
-        if frame == i then
-          _accum_0[_len_0] = composeCarrots(arg, frames)
-        else
-          _accum_0[_len_0] = composeTerm(arg)
-        end
-        _len_0 = _len_0 + 1
-      end
-      args = _accum_0
-    end
-    local optargs = { }
-    for key, arg in ipairs(term.optargs) do
-      if frame == key then
-        optargs[key] = composeCarrots(arg, frames)
-      else
-        optargs[key] = composeTerm(arg)
-      end
-    end
-    if frame then
-      return term.compose(args, optargs)
-    else
-      return carrotify(term.compose(args, optargs))
-    end
-  end
-  carrotMarker = { }
-  carrotify = function(tree)
-    return {
-      carrotMarker,
-      tree
-    }
-  end
-  joinTree = function(tree)
-    local str = ''
-    for _, term in ipairs(tree) do
-      if Array.isArray(term) then
-        if term.length == 2 and term[0] == carrotMarker then
-          str = str + joinTree(term[1]).gsub('.', '^')
-        else
-          str = str + joinTree(term)
-        end
-      else
-        str = str + term
-      end
-    end
-    return str
-  end
   RqlQueryPrinter = _class_0
 end
 return {
@@ -261,5 +247,4 @@ return {
   RqlRuntimeError = RqlRuntimeError,
   RqlCompileError = RqlCompileError,
   RqlClientError = RqlClientError,
-  printQuery = RqlQueryPrinter.printQuery
 }
