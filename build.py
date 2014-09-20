7b6a536cb91d60d4cf088903c9ff88d32a927faa
Adam Grandquist <grandquista@gmail.com>
2014-09-19 20:01:58 -0700

    Update the Python build process to work with luarocks.

--- build.py
+++ build.py
@@ -33,7 +33,6 @@ skipped: {}'''.format(
 def clean(args):
     shutil.rmtree('build', ignore_errors=True)
     shutil.rmtree('tests/__pycache__', ignore_errors=True)
-    subprocess.call(['git', 'submodule', 'update'])
 
 
 def lint(args):
@@ -41,43 +40,11 @@ def lint(args):
 
 
 def build(args):
-    debug = os.getenv('DEBUG')
-    plat = os.getenv('PLAT', args.plat)
+    pass
 
-    incl = os.getenv('LUAINC', args.incl)
-    build = os.getenv('LUAPREFIX', args.build)
 
-    incl_macosx = os.getenv('LUAINC_macosx_base', incl)
-    build_macosx = os.path.join('..', '..', os.getenv('LUAPREFIX_macosx', build))
-
-    incl_linux = os.getenv('LUAINC_linux_base', incl)
-    build_linux = os.path.join('..', '..', os.getenv('LUAPREFIX_linux', build))
-
-    build_dir = os.path.join(
-        'luasocket', 'src',
-        build_macosx if args.plat == 'macosx' else build_linux
-    )
-
-    try:
-        os.mkdir(build_dir)
-    except OSError:
-        print('luasocket build directory {} already created.'.format(build_dir))
-
-    cmd = ['make', 'install-both']
-    env = dict(
-        os.environ,
-        PLAT=plat,
-        DEBUG=debug or '',
-        **({
-            'LUAINC_macosx_base': incl_macosx,
-            'LUAPREFIX_macosx': build_macosx
-        } if args.plat == 'macosx' else {
-            'LUAINC_linux_base': incl_linux,
-            'LUAPREFIX_linux': build_linux
-        })
-    )
-
-    with subprocess.Popen(cmd, cwd='luasocket', env=env) as io:
+def install(args):
+    with subprocess.Popen(['luarocks', 'make']) as io:
         if io.wait():
             print(repr(cmd), "returned:", io.returncode)
             exit(io.returncode)
@@ -86,12 +53,7 @@ def build(args):
 def main():
     parser = argparse.ArgumentParser(description='Process some integers.')
 
-    platform = 'macosx' if sys.platform.startswith('darwin') else 'linux'
-
     parser.add_argument('action', nargs='?', default='build')
-    parser.add_argument('-p', '--plat', default=platform)
-    parser.add_argument('-i', '--incl', default='/usr/local/include')
-    parser.add_argument('-b', '--build', default='build')
     parser.add_argument('-j', type=int)
 
     args = parser.parse_args()
@@ -100,6 +62,7 @@ def main():
         'test': test,
         'lint': lint,
         'clean': clean,
+        'install': install,
         'build': build
     }[args.action](args)
 
