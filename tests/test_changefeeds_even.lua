41190e31cb2db1d00a03645e403aa2d4231705f6
Adam Grandquist <grandquista@gmail.com>
2014-09-22 19:33:36 -0700

    Fix calls to `each` in tests.

--- tests/test_changefeeds_even.lua
+++ tests/test_changefeeds_even.lua
@@ -8,7 +8,7 @@ r.connect(function(err, c)
       r.db('changefeeds'):table('watched'):insert(
         {{id = 7}, {id = 8}, {id = 9}, {id = 10}}
       ):run(c)
-      cur.each(function(row)
+      cur:each(function(row)
         print(row.new_val.id)
       end)
     end
