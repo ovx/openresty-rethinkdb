ad6837b2c279d5819af396dd4f5bf96a9de16a07
Adam Grandquist <grandquista@gmail.com>
2014-09-24 18:32:29 -0700

    Rewrite `next` and friends for the new interface.

--- src/cursor.lua
+++ src/cursor.lua
@@ -7,6 +7,7 @@ do
   local _base_0 = {
     stack_size = 100,
     _add_response = function(self, response)
+      if not self._type then self._type = response.t end
       if response.t == self._type or response.t == proto_response_type.SUCCESS_SEQUENCE then
         -- We push a "ok" response only if it's not empty
         if #response.r > 0 then
@@ -15,7 +16,6 @@ do
       else
         table.insert(self._responses, response)
       end
-      self._outstanding_requests = self._outstanding_requests - 1
       if response.t ~= self._type then
         -- We got an error or a SUCCESS_SEQUENCE
         self._end_flag = true
@@ -126,100 +126,91 @@ do
       end
     end,
     _prompt_cont = function(self)
+      if self._end_flag then return end
       -- Let's ask the server for more data if we haven't already
-      if not ((self._cont_flag or self._end_flag)) then
+      if not self._cont_flag then
         self._cont_flag = true
-        self._outstanding_requests = self._outstanding_requests + 1
         self._conn:_continue_query(self._token)
       end
       self._conn:_get_response(self._token)
     end,
     -- Implement IterableResult
-    _next = function(self, cb)
-      local fn = function(self, cb)
-        self._cb_queue.push(cb)
-        return self:_prompt_next()
+    next = function(self, cb)
+      -- Try to get a row out of the responses
+      if not self._responses[1] then
+        -- We prefetch things here, set `is 0` to avoid prefectch
+        if self._end_flag == true then
+          return cb(err.ReQLDriverError("No more rows in the cursor."))
+        end
+        self:_prompt_cont()
       end
-      return fn(cb)
+      local response = self._responses[1]
+      -- Error responses are not discarded, and the error will be sent to all future callbacks
+      local t = response.t
+      if proto_response_type.SUCCESS_PARTIAL == t or proto_response_type.SUCCESS_FEED == t or proto_response_type.SUCCESS_SEQUENCE == t then
+        local row = err.recursively_convert_pseudotype(response.r[self._response_index], self._opts)
+        self._response_index = self._response_index + 1
+
+        -- If we're done with this response, discard it
+        if not response.r[self._response_index] then
+          table.remove(self._responses, 1)
+          self._response_index = 1
+        end
+        return cb(nil, row)
+      elseif proto_response_type.COMPILE_ERROR == t then
+        return cb(err.ReQLCompileError(response, self._root))
+      elseif proto_response_type.CLIENT_ERROR == t then
+        return cb(err.ReQLClientError(response, self._root))
+      elseif proto_response_type.RUNTIME_ERROR == t then
+        return cb(err.ReQLRuntimeError(response, self._root))
+      end
+      return cb(err.ReQLDriverError("Unknown response type for cursor"))
     end,
     close = function(self, cb)
-      if self._end_flag == true then
-        return cb()
-      else
-        self._close_cb = cb
-        if self._outstanding_requests > 0 then
-          self._close_asap = true
-        else
-          self._outstanding_requests = self._outstanding_requests + 1
-          return self._conn._end_query(self._token)
-        end
+      if not self._end_flag then
+        self._conn:_end_query(self._token)
       end
+      if cb then return cb() end
     end,
-    _each = function(self, cb, on_finished)
-      if not (type(cb) == 'function') then
+    each = function(self, cb, on_finished)
+      if type(cb) ~= 'function' then
         error(err.ReQLDriverError("First argument to each must be a function."))
       end
       if on_finished and type(on_finished) ~= 'function' then
         error(err.ReQLDriverError("Optional second argument to each must be a function."))
       end
-      local stop_flag = false
-      local self = self
-      local next_cb = function(self, err, data)
-        if stop_flag ~= true then
-          if err then
-            if err.message == 'No more rows in the cursor.' then
-              if on_finished then
-                return on_finished()
-              end
-            else
-              return cb(err)
-            end
-          else
-            stop_flag = cb(nil, data) == false
-            return self:_next(next_cb)
+      function next_cb(err, data)
+        if err then
+          if err.message ~= 'No more rows in the cursor.' then
+            return cb(err)
           end
-        else
           if on_finished then
             return on_finished()
           end
+        else
+          cb(nil, data)
+          return self:next(next_cb)
         end
       end
-      return self:_next(next_cb)
+      return self:next(next_cb)
     end,
     to_array = function(self, cb)
-      local fn = function(self, cb)
-        local arr = { }
-        local each_cb = function(self, err, row)
+      if not self._type then self:_prompt_cont() end
+      if self._type == proto_response_type.SUCCESS_FEED then
+        return cb(err.ReQLDriverError("`to_array` is not available for feeds."))
+      end
+      local arr = {}
+      return self:each(
+        function(err, row)
           if err then
             return cb(err)
-          else
-            return arr.push(row)
           end
-        end
-        local on_finish = function(self, err, ar)
+          table.insert(arr, row)
+        end,
+        function()
           return cb(nil, arr)
         end
-        return self:each(each_cb, on_finish)
-      end
-      return fn(cb)
-    end,
-    _make_emitter = function(self)
-      self.emitter = EventEmitter
-      self.each = function()
-        error(err.ReQLDriverError("You cannot use the cursor interface and the EventEmitter interface at the same time."))
-      end
-      self.next = function()
-        error(err.ReQLDriverError("You cannot use the cursor interface and the EventEmitter interface at the same time."))
-      end
-    end,
-    add_listener = function(self, ...)
-      if not self.emitter then
-        self:_make_emitter()
-        set_immediate(function(self)
-          return self:_each(self._each_cb)
-        end)
-      end
-      return self.emitter.add_listener(self, ...)
+      )
     end,
   }
   _base_0.__index = _base_0
@@ -230,16 +221,14 @@ do
       self._opts = opts
       self._root = root -- current query
       self._responses = { }
-      self._response_index = 0
       self._outstanding_requests = 1 -- Because we haven't add the response yet
       self._iterations = 0
+      self._response_index = 1
       self._end_flag = false
       self._cont_flag = false
       self._close_asap = false
       self._cont = nil
       self._cb_queue = { }
-      self.next = self._next
-      self.each = self._each
     end,
     __base = _base_0,
     __name = "Cursor"
