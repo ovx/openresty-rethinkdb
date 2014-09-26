00817008c53006b73ae336c56e6879297e47691e
Adam Grandquist <grandquista@gmail.com>
2014-09-25 18:28:26 -0700

    Simplify test nameing.

--- tests/test_changefeeds.py
+++ tests/test_changefeeds.py
@@ -8,10 +8,10 @@ class TestChangeFeeds(util.LuaTestCase):
             {'id': 4}, {'id': 5}, {'id': 6}
         ])
 
-    def test_all_changes(self):
+    def test_all(self):
         self.assertEqual(self.run_lua('test_changefeeds_all'), "7\n8\n9\n10\n")
 
-    def test_even_changes(self):
+    def test_even(self):
         self.assertEqual(self.run_lua('test_changefeeds_even'), "8\n10\n")
 
 
