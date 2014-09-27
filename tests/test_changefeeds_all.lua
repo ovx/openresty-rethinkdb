827d365a402f0954b5c020f5eb0064106313728f
Adam Grandquist <grandquista@gmail.com>
2014-09-26 20:46:07 -0700

    Add more information to test failures.

--- tests/test_changefeeds_all.lua
+++ tests/test_changefeeds_all.lua
@@ -3,10 +3,12 @@ r = require('rethinkdb')
 r.connect(function(err, c)
   r.db('changefeeds'):table('watched'):changes():limit(4):run(
     c, function(err, cur)
+      if err then error(err.message) end
       r.db('changefeeds'):table('watched'):insert(
         {{id = 7}, {id = 8}, {id = 9}, {id = 10}}
       ):run(c)
       cur:each(function(err, row)
+        if err then error(err.message) end
         print(row.new_val.id)
       end)
     end
