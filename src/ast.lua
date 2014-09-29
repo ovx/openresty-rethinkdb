baba846963c5cf1e1d977f1a80b321496cdf0163
Adam Grandquist <grandquista@gmail.com>
2014-09-28 18:30:49 -0700

    Fix code indentation.

--- src/ast.lua
+++ src/ast.lua
@@ -676,13 +676,13 @@ do
         }
       else
         if self.args then
-        if should_wrap(self.args[1]) then
-          args[1] = {
-            'r.expr(',
-            args[1],
-            ')'
-          }
-        end
+          if should_wrap(self.args[1]) then
+            args[1] = {
+              'r.expr(',
+              args[1],
+              ')'
+            }
+          end
         end
         return {
           args[1],
