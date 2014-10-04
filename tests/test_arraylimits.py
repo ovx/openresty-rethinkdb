186a0ba586e280425fb98d59649b92290a2f1c9b
Adam Grandquist <grandquista@gmail.com>
2014-10-03 17:58:02 -0700

    Change expected failure messages in array limits test.

--- tests/test_arraylimits.py
+++ tests/test_arraylimits.py
@@ -6,7 +6,7 @@ class TestArrayLimits(util.LuaTestCase):
         self.create_table('array.limits')
 
     def test_create(self):
-        self.assertEqual(self.run_lua('test_arraylimits_create'), "err(RqlRuntimeError, Array over size limit `4`., [0])")
+        self.assertEqual(self.run_lua('test_arraylimits_create'), "ReQLRuntimeError Array over size limit `4`. in:\n{1, 2, 3, 4, 5, 6, 7, 8}")
 
     def test_equal(self):
         self.assertEqual(self.run_lua('test_arraylimits_equal'), "{1, 2, 3, 4, 5, 6, 7, 8}")
@@ -21,16 +21,16 @@ class TestArrayLimits(util.LuaTestCase):
         self.assertEqual(self.run_lua('test_arraylimits_huge_table'), "{'deleted':0.0,'replaced':0.0,'unchanged':0.0,'errors':1.0,'skipped':0.0,'inserted':1,'first_error':Array too large for disk writes (limit 100,000 elements)}")
 
     def test_lessthan(self):
-        self.assertEqual(self.run_lua('test_arraylimits_lessthan'), "err(RqlRuntimeError, Array over size limit `4`., [0])")
+        self.assertEqual(self.run_lua('test_arraylimits_lessthan'), "ReQLRuntimeError Array over size limit `4`. in:\nr({1, 2, 3, 4}):union({5, 6, 7, 8})")
 
     def test_lessthan_read(self):
         self.assertEqual(self.run_lua('test_arraylimits_lessthan_read'), "{'array':[1,2,3,4,5,6,7,8,9,10],'id':1}")
 
     def test_negative(self):
-        self.assertEqual(self.run_lua('test_arraylimits_negative'), "err(RqlCompileError, Illegal array size limit `-1`., [])")
+        self.assertEqual(self.run_lua('test_arraylimits_negative'), "ReQLCompileError Illegal array size limit `-1`. in:\n{1, 2, 3, 4, 5, 6, 7, 8}")
 
     def test_zero(self):
-        self.assertEqual(self.run_lua('test_arraylimits_zero'), "err(RqlCompileError, Illegal array size limit `0`., [])")
+        self.assertEqual(self.run_lua('test_arraylimits_zero'), "ReQLCompileError Illegal array size limit `0`. in:\n{1, 2, 3, 4, 5, 6, 7, 8}")
 
 
 if __name__ == '__main__':
