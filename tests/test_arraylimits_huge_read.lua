120d25923cec71e329056e573c87b4ab2778105b
Adam Grandquist <grandquista@gmail.com>
2014-10-03 17:58:41 -0700

    Fix missing `:` in tests.

--- tests/test_arraylimits_huge_read.lua
+++ tests/test_arraylimits_huge_read.lua
@@ -3,7 +3,7 @@ r = require('rethinkdb')
 r.connect({timeout = 1}, function(err, c)
   ten_l = r.expr({1, 2, 3, 4, 5, 6, 7, 8, 9, 10})
   huge_l = ten_l:concat_map(ten_l):concat_map(ten_l):concat_map(ten_l):concat_map(ten_l)
-  r.db():table():insert({id = 0, array = huge_l.append(1)}):run(
+  r.db('array'):table('limits'):insert({id = 0, array = huge_l:append(1)}):run(
     c, {array_limit = 100001}, function(err, cur)
       if err then error(err.message) end
       cur:to_array(function(err, arr)
