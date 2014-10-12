e4ce4ac18d1372f76aa9c298f13edde16d977c71
Adam Grandquist <grandquista@gmail.com>
2014-10-12 13:28:52 -0700

    Three more expected results in tests need a list wrap.

--- tests/test_arraylimits.py
+++ tests/test_arraylimits.py
@@ -15,16 +15,16 @@ class TestArrayLimits(util.LuaTestCase):
         self.expect('test_arraylimits_huge', [100001])
 
     def test_huge_read(self):
-        self.expect('test_arraylimits_huge_read', [None])
+        self.expect('test_arraylimits_huge_read', {})
 
     def test_huge_table(self):
-        self.expect('test_arraylimits_huge_table', {'deleted': 0, 'errors': 1, 'first_error': 'Array too large for disk writes (limit 100,000 elements)', 'inserted': 1, 'replaced': 0, 'skipped': 0, 'unchanged': 0})
+        self.expect('test_arraylimits_huge_table', [{'deleted': 0, 'errors': 1, 'first_error': 'Array too large for disk writes (limit 100,000 elements)', 'inserted': 1, 'replaced': 0, 'skipped': 0, 'unchanged': 0}])
 
     def test_lessthan(self):
         self.expect('test_arraylimits_lessthan', 'ReQLRuntimeError Array over size limit `4`. in:\nr({1, 2, 3, 4}):union({5, 6, 7, 8})')
 
     def test_lessthan_read(self):
-        self.expect('test_arraylimits_lessthan_read', {'array': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 'id': 1})
+        self.expect('test_arraylimits_lessthan_read', [{'array': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 'id': 1}])
 
     def test_negative(self):
         self.expect('test_arraylimits_negative', 'ReQLCompileError Illegal array size limit `-1`. in:\n{1, 2, 3, 4, 5, 6, 7, 8}')
