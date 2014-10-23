d6100ee0b651c4ddd43834e01be7a2d2d94bc09f
Adam Grandquist <grandquista@gmail.com>
2014-10-22 17:11:47 -0700

    Change feeds need results to be sorted.

--- spec/changefeeds_spec.lua
+++ spec/changefeeds_spec.lua
@@ -29,50 +29,50 @@ describe('change feeds', function()
   end)
 
   it('all', function()
-    assert.are.equal(
-      r.table(reql_table):changes():limit(4):run(
-        c, function(err, cur)
+    local res = r.table(reql_table):changes():limit(4):run(
+      c, function(err, cur)
+        if err then error(err.message) end
+        r.table(reql_table):insert(
+          {{id = 7}, {id = 8}, {id = 9}, {id = 10}}
+        ):run(c, function(err)
           if err then error(err.message) end
-          r.table(reql_table):insert(
-            {{id = 7}, {id = 8}, {id = 9}, {id = 10}}
-          ):run(c, function(err)
-            if err then error(err.message) end
-          end)
-          local res = {}
-          cur:each(function(row)
-            table.insert(res, row.new_val.id)
-          end, function(err)
-            if err then error(err.message) end
-          end)
-          return res
-        end
-      ), {7, 8, 9, 10}
+        end)
+        local res = {}
+        cur:each(function(row)
+          table.insert(res, row.new_val.id)
+        end, function(err)
+          if err then error(err.message) end
+        end)
+        return res
+      end
     )
+    table.sort(res)
+    assert.same(res, {7, 8, 9, 10})
   end)
 
   it('even', function()
-    assert.are.equal(
-      r.table(reql_table):changes():filter(
-        function(row)
-          return (row('new_val')('id') % 2):eq(0)
-        end
-      ):limit(2):run(
-        c, function(err, cur)
+    local res = r.table(reql_table):changes():filter(
+      function(row)
+        return (row('new_val')('id') % 2):eq(0)
+      end
+    ):limit(2):run(
+      c, function(err, cur)
+        if err then error(err.message) end
+        r.table(reql_table):insert(
+          {{id = 7}, {id = 8}, {id = 9}, {id = 10}}
+        ):run(c, function(err)
           if err then error(err.message) end
-          r.table(reql_table):insert(
-            {{id = 7}, {id = 8}, {id = 9}, {id = 10}}
-          ):run(c, function(err)
-            if err then error(err.message) end
-          end)
-          local res = {}
-          cur:each(function(row)
-            table.insert(res, row.new_val.id)
-          end, function(err)
-            if err then error(err.message) end
-          end)
-          return res
-        end
-      ), {8, 10}
+        end)
+        local res = {}
+        cur:each(function(row)
+          table.insert(res, row.new_val.id)
+        end, function(err)
+          if err then error(err.message) end
+        end)
+        return res
+      end
     )
+    table.sort(res)
+    assert.same(res, {8, 10})
   end)
 end)
